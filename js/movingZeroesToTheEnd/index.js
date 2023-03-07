102a9da0e62ed9ce938b53f1807565fdc76b7a9c
Pavel <assimpusx@yandex.ru>
2023-03-07 20:13:08 +0700

    solve move kata

--- js/movingZeroesToTheEnd/index.js
+++ js/movingZeroesToTheEnd/index.js
@@ -1,3 +1,16 @@
 function moveZeros(arr) {
-    return
+    const sortArr= [];
+    let counter = 0;
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] !== 0) {
+            sortArr.push(arr[i]);
+        } else {
+            counter++;
+        }
+    }
+    for(let i = 0; i < counter; i++) {
+        sortArr.push(0);
+    }
+
+    return sortArr;
 }
