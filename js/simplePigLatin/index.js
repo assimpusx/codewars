e799539ece173e806a9d21c8c90286754512ab5d
Pavel <assimpusx@yandex.ru>
2023-03-10 20:46:55 +0700

    solve pig latin kata

--- js/simplePigLatin/index.js
+++ js/simplePigLatin/index.js
@@ -1,3 +1,12 @@
 function pigIt(str){
-  //Code here
+    return str
+        .split(' ')
+        .map(el => {
+            if((/[a-z]/gi).test(el)) {
+               return el.slice(1, el.length) + el.slice(0, 1) + 'ay'
+            } else {
+                return el;
+            }
+        })
+        .join(' ')
 }
