c2b1faa283d4973d045e0dc6f5ea51fd7b86065f
Pavel <assimpusx@yandex.ru>
2023-03-14 21:45:05 +0700

    add roman obj

--- js/romanNumeralsHelper/index.js
+++ js/romanNumeralsHelper/index.js
@@ -1,5 +1,16 @@
 class RomanNumerals {
   static toRoman(num) {
+    const romanValues = {
+      1: 'I',
+      5: 'V',
+      10: 'X',
+      50: 'L',
+      100: 'C',
+      500: 'D',
+      1000: 'M'
+    }
+
+    
     return 'IV';
   }
 
